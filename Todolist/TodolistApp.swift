79b95c605706bd43d15075bbf27461ed1a48df7e
Hussam ahtash <Hussamahtash11@gmail.com>
2023-11-20 11:15:10 +0100

    Initial Commit

---
+++ Todolist/TodolistApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodolistApp.swift
+//  Todolist
+//
+//  Created by devsEfusion on 20/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct TodolistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
